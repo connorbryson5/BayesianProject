763368f557b3e4f3cef47e86a11efede7442c3c4
CalebSkinner1 <99994615+CalebSkinner1@users.noreply.github.com>
2022-05-01 20:20:18 -0500

    Add files via upload

--- WineData.R
+++ WineData.R
@@ -3,6 +3,9 @@ view(df)
 library(tidyverse)
 library(ggplot2)
 library(plyr)
+library(ProbBayes)
+library(rjags)
+
 #bayesian and frequentist approach of t.test of citric acid effect
 
 #df with citric acid and quality; citric_zero = 0 when zero citric acid, 1 when citric acid
@@ -24,7 +27,63 @@ ggplot(df_citric, aes(x=quality, color=citric_zero, fill = citric_zero)) +
              color="darkgreen", linetype="dashed", size=1)
 
 # mean of both
-mean(df_citric$quality[df_citric$citric_zero == 0])
+mu0 <- mean(df_citric$quality[df_citric$citric_zero == 0])
 #5.44
-mean(df_citric$quality[df_citric$citric_zero == 1])
+mu1 <- mean(df_citric$quality[df_citric$citric_zero == 1])
 #5.68
+mean_overall <- mean(df_citric$quality)
+#5.66
+sigma0 <- sd(df_citric$quality[df_citric$citric_zero == 0])/sqrt(length(citric_0))
+#.703
+sigma1 <- sd(df_citric$quality[df_citric$citric_zero == 1])/sqrt(length(citric_1))
+#.812
+
+citric_1 <- df_citric %>%
+  filter(citric_zero == 1) %>%
+  select(quality) %>%
+  view()
+citric_0 <- df_citric %>%
+  filter(citric_zero == 0) %>%
+  select(quality) %>%
+  view()
+
+cat("
+model {
+## sampling
+
+for(i in 1:n0){
+y_0[i] ~ dnorm(mu_0, phi0)
+}
+for(i in 1:n1){
+y_1[i] ~ dnorm(mu_1, phi1)
+}
+## priors
+
+mu_0 ~ dnorm(0, .0001)
+mu_1 ~ dnorm(0, .0001)
+phi0 ~ dgamma(.1,.1)
+phi1 ~ dgamma(.1,.1)
+
+##interests
+
+diff <- mu_0 - mu_1
+#ratio <- mu_0/mu_1
+}
+", file = "normal-normal.jags")
+
+
+n0 = 99
+n1 = 1044
+
+inits <- list(mu_0 = 1, mu_1 = 1, phi0 = .1, phi1 = .1)
+data <- list("y_0" = citric_0$quality, "y_1" = citric_1$quality, "n0" = n0, "n1" = n1)
+
+
+jm <- jags.model("normal-normal.jags", data = data, quiet = TRUE)
+
+samps <- coda.samples(jm, variable.names = c("diff"), n.iter = 10000)
+
+ests <- summary(samps, quantiles = c(.025, .975))
+ests
+mcmc_hist(samps)
+mcmc_areas(samps, "mu", prob = .95, point.est = "mean") + labs(x = expression(mu), title = "Posterior")
